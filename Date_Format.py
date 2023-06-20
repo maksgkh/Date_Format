bb6d118fbe91ab7592fe4c69f5d5efcae7ca6780
maksgkh <81386044+maksgkh@users.noreply.github.com>
2023-06-21 00:57:26 +0300

    Update Date_Format.py

--- Date_Format.py
+++ Date_Format.py
@@ -16,10 +16,7 @@ def Date_Format(date):
     result_date = new_date[-4] + new_date[-3] +new_date[-2] +new_date[-1] + new_date[2] +new_date[3] +new_date[0] +new_date[1] 
     print(result_date)
 
-
-
-
-
+#Check Dates
 Date_Format("11/12/2019")
 Date_Format("12/31/2019")
 Date_Format("12/31/2019")
